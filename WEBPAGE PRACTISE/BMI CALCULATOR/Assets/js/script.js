1695be1139440040ce623b5e9eacf727c40a8976
Abishek-CSE <abisheknavas500@gmail.com>
2025-12-01 19:31:17 +0530

    'react'

--- WEBPAGE PRACTISE/BMI CALCULATOR/Assets/js/script.js
+++ WEBPAGE PRACTISE/BMI CALCULATOR/Assets/js/script.js
@@ -7,4 +7,18 @@ function bmicalculator(){
     let height = HeightCM / 100
     let BMI =Weight / (height*height) 
     document.getElementById("result").innerHTML=`Your BMI is ${BMI.toFixed(2)}`
+
+    if (BMI <= 18) {
+        alert("UnderWeight");
+    }
+    else if (BMI > 18 && BMI <= 25) {
+        alert("Healthy Weight");
+    }
+    else if (BMI > 25 && BMI < 30) {
+        alert("OverWeight");
+    }
+    else {
+        alert("Obese");
+    }
 }
+
